b086bc00b3711f3d7b6aa60d8cddb4725c91a5c2
PetuchovArtem <petuchov.artem@gmail.com>
2021-07-15 19:11:48 +0300

    Update MathTest1.java
    change tests value

--- src/test/java/by/academy/junit/MathTest1.java
+++ src/test/java/by/academy/junit/MathTest1.java
@@ -10,7 +10,7 @@ public class MathTest1 extends Assert {
 		
 	@Test
 	public void testPow() {
-		assertTrue(Math.pow(2, 1) == 2);
+		assertTrue(Math.pow(12, 1) == 13);
 		assertTrue(Math.pow(2, 2) == 4);
 		assertTrue(Math.pow(2, 3) == 8);
 		assertTrue(Math.pow(2, 4) == 16);
